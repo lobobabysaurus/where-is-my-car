9995790ee5180b40bb8adf4323126437d9d38f4a
Phil Simmons <phil@tubbycatgames.com>
2018-10-14 09:01:56 -0400

    Added Basic Tracking Functionality

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Beggining of the app!</Text>
-      </View>
-    );
-  }
-}
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+  },
+  text: {
+    color: '#fff',
+    minWidth: '35%',
+    textAlign: 'center',
   },
 });
+
+export default class App extends React.Component {
+
+  state = {
+    coords: undefined
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>{this._formatCoordinates()}</Text>
+        <Button
+          color='purple'
+          onPress={this._onButtonClick}
+          title='Mark'
+        />
+      </View>
+    );
+  }
+
+  _formatCoordinates = _ => {
+    const coords = this.state.coords;
+    if (coords) {
+      return `${coords.latitude},${coords.longitude}`;
+    } else {
+      return "No Coordinates";
+    }
+  }
+
+  _onButtonClick = _ => {
+    navigator.geolocation.getCurrentPosition(
+      this._onGeoSuccess,
+      this._onGeoError);
+  }
+
+  _onGeoSuccess = (position) => {
+    this.setState({coords: position.coords});
+  }
+
+  _onGeoError(err) {
+    Alert.alert(`Error getting location: ${err}`);
+  }
+
+}
