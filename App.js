2bac7e85c14fb1284c322a06772bcd5199c41dc5
Phil Simmons <phil@tubbycatgames.com>
2018-10-14 23:06:49 -0400

    Decomposed App.js into a series of isolated files

--- App.js
+++ App.js
@@ -1,49 +1,8 @@
 import React from 'react'
-import { Alert, AsyncStorage, StyleSheet, TouchableHighlight, Text, View } from 'react-native'
-import MapView from 'react-native-maps'
+import { Alert, View } from 'react-native'
 
-const styles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    backgroundColor: 'purple',
-    height: '10%',
-    justifyContent: 'space-evenly',
-    width: '100%',
-  },
-  map: {
-    height: '90%',
-    width: '100%',
-  },
-  text: {
-    color: 'white',
-    fontSize: 36,
-  },
-})
-
-const saveCoords = async coord => {
-  try {
-    await AsyncStorage.setItem('latitude', coord.latitude.toString())
-    await AsyncStorage.setItem('longitude', coord.longitude.toString())
-  } catch (err) {
-    Alert.alert(err.message)
-  }
-}
-
-const getCoords = async () => {
-  try {
-    const latitude = await AsyncStorage.getItem('latitude')
-    const longitude = await AsyncStorage.getItem('longitude')
-    if (latitude && longitude) {
-      return {
-        latitude: parseFloat(latitude),
-        longitude: parseFloat(longitude),
-      }
-    }
-  } catch (err) {
-    Alert.alert(`Error getting coordinate: ${err}`)
-  }
-  return undefined
-}
+import { getCoords, saveCoords } from './lib/coords'
+import { MarkButton, MarkedMap } from './lib/components'
 
 export default class App extends React.Component {
 
@@ -69,30 +28,13 @@ export default class App extends React.Component {
   render() {
     return (
       <View>
-        <MapView style={styles.map} initialRegion={this.region}>
-          {this._marker()}
-        </MapView>
-        <TouchableHighlight style={styles.button} onPress={this._onButtonClick}>
-          <Text style={styles.text}>Mark</Text>
-        </TouchableHighlight>
+        <MarkedMap coords={this.state.coords} region={this.region}/>
+        <MarkButton onPress={this._updateLocation} />
       </View>
     )
   }
 
-  _marker = () => {
-    const coords = this.state.coords
-    if (coords) {
-      return <MapView.Marker
-        coordinate={coords}
-        title="Car"
-        description="You Parked Here!"
-      />
-    } else {
-      return undefined
-    }
-  }
-
-  _onButtonClick = () => {
+  _updateLocation = () => {
     navigator.geolocation.getCurrentPosition(
       position => {
         const coords = position.coords
