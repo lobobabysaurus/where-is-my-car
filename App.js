91a6c85a23ea45bcbbcfc7b4861b7ba8da16afc1
Phil Simmons <phil@tubbycatgames.com>
2018-12-09 17:49:55 -0500

    Took boilerplate out of route registration

--- App.js
+++ App.js
@@ -4,9 +4,15 @@ import * as routes from './lib/routes'
 import HistoryScreen from './lib/containers/HistoryScreen'
 import MapScreen from './lib/containers/MapScreen'
 
-const screens = {}
-screens[routes.HISTORY] = HistoryScreen
-screens[routes.MAP] = MapScreen
+const screens = [
+  [routes.HISTORY, HistoryScreen],
+  [routes.MAP, MapScreen],
+].reduce(
+  (map, [route, screen]) => {
+    map[route] = screen
+    return map
+  },
+  {})
 const AppNavigator = createStackNavigator(screens, {
   initialRouteName: 'Map'
 })
